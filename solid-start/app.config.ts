4feefa5fb81a26f62dae80fe02e005962da950f3
Kenji Saito <ken-yo@mbr.nifty.com>
2026-04-07 22:38:41 +0900

    Bumps node modules

--- solid-start/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@solidjs/start/config';
-
-export default defineConfig({
-  server: {
-    preset: 'aws-lambda',
-    serveStatic: true,
-  },
-});
--- solid-start/src/entry-client.tsx
+++
@@ -1,7 +0,0 @@
-// @refresh reload
-import { mount, StartClient } from '@solidjs/start/client';
-
-const app = document.getElementById('app');
-if (app) {
-  mount(() => <StartClient />, app);
-}
