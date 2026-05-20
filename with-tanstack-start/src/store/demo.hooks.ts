e47fcad0ceeaf79389f437b2ebf7d5152cbed426
Kenji Saito <ken-yo@mbr.nifty.com>
2026-05-20 22:12:39 +0900

    Add the TanStack Start example

---
+++ with-tanstack-start/src/store/demo.hooks.ts
@@ -0,0 +1,17 @@
+import { useSelector } from '@tanstack/solid-store'
+import { store, actions, selectors } from './demo.store'
+
+export type { State, Prompt, Conversation } from './demo.store'
+
+export function useAppState() {
+  const state = useSelector(store)
+  return state
+}
+
+export function useAppActions() {
+  return actions
+}
+
+export function useAppSelectors() {
+  return selectors
+}
